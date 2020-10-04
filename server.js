01642c06c7293665a51deec44bdb804b96785b51
David Acosta <acosta816@gmail.com>
2020-10-04 16:36:57 -0700

    added all /shoppinglist crud endpoints

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const morgan = require('morgan');
 // interact with in our GET endpoint
 const { ShoppingList, Recipes } = require('./models');
 
-// const jsonParser = bodyParser.json();
 const app = express();
 
 // log the http layer
@@ -30,11 +29,77 @@ Recipes.create("Nata's Vegan Horchata", ['dates', 'rice', 'milk cloth', 'cinnamo
 Recipes.create("Oatmeal", ['milk', 'water', 'oats', 'brown sugar', 'apples']);
 Recipes.create("Narb's Red things", ['fear', 'red', 'things']);
 
-// when the root of this route is called with GET, return
-// all current ShoppingList items by calling `ShoppingList.get()`
+
+
+
+
+
+// when the root of this router is called with GET, return
+// all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
 });
+
+app.post('/shopping-list', jsonParser, (req, res) => {
+  // ensure `name` and `budget` are in request body
+  const requiredFields = ['name', 'budget'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  const item = ShoppingList.create(req.body.name, req.body.budget);
+  res.status(201).json(item);
+});
+
+// when PUT request comes in with updated item, ensure has
+// required fields. also ensure that item id in url path, and
+// item id in updated item object match. if problems with any
+// of that, log error and send back status code 400. otherwise
+// call `ShoppingList.update` with updated item.
+app.put('/shopping-list/:id', jsonParser, (req, res) => {
+  const requiredFields = ['name', 'budget', 'id'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  if (req.params.id !== req.body.id) {
+    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating shopping list item \`${req.params.id}\``);
+  ShoppingList.update({
+    id: req.params.id,
+    name: req.body.name,
+    budget: req.body.budget
+  });
+  res.status(204).end();
+});
+
+// when DELETE request comes in with an id in path,
+// try to delete that item from ShoppingList.
+app.delete('/shopping-list/:id', (req, res) => {
+  ShoppingList.delete(req.params.id);
+  console.log(`Deleted shopping list item \`${req.params.ID}\``);
+  res.status(204).end();
+});
+
+
+
+/**====================================================================================================END of SHOPPINGLIST ROUTES=============================== */
+
+
+
 
 app.get('/recipes', (req, res) => {
   res.status(200).json(Recipes.get());
@@ -72,6 +137,11 @@ app.put('/recipes/:id', (req, res) => {
   Recipes.update(req.body);
   res.status(204).end();
 })
+
+
+
+
+
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
